16076ef28a37ef63d67e073f794a85eb006b60f8
AlexAaqil <alexaaqil.se@gmail.com>
2026-07-04 13:01:01 +0300

    finished the basic structure for the homepage.

---
+++ src/data/app.config.ts
@@ -0,0 +1,79 @@
+export const AppConfig = {
+  categorizedLinks: [
+    {
+        category: "AI",
+        links: [
+            { name: "ChatGPT", url: "https://chatgpt.com/" },
+            { name: "Deepseek", url: "https://chat.deepseek.com/" },
+            { name: "Claude", url: "https://claude.ai/" },
+            { name: "Qwen", url: "https://chat.qwenlm.ai/" },
+            { name: "Kimi AI", url: "https://kimi.ai/chat/cuu3lhqsc5cj3jqd9p3g" },
+            { name: "MGX", url: "https://mgx.dev/chat/5iof8d" },
+        ]
+    },
+    {
+        category: "E-Learning",
+        links: [
+            { name: "Duolingo", url: "https://duolingo.com" },
+        ]
+    },
+    {
+        category: "Chess",
+        links: [
+            { name: "chess.com", url: "https://www.chess.com" },
+            { name: "Lichess", url: "https://lichess.org/" },
+            { name: "Chessable", url: "https://www.chessable.com/" },
+        ]
+    },
+    {
+        category: "Socials",
+        links: [
+            { name: "Gmail", url: "https://mail.google.com/" },
+            { name: "YouTube", url: "https://youtube.com" },
+            { name: "TikTok", url: "https://www.tiktok.com/" },
+            { name: "Github", url: "https://github.com/" },
+        ]
+    },
+    {
+        category: "Docs",
+        links: [
+            { name: "Tailwindcss", url: "https://tailwindcss.com/docs" },
+            { name: "Shadcn-Vue", url: "https://www.shadcn-vue.com/" },
+            { name: "Daraja-API", url: "https://developer.safaricom.co.ke/account/login" },
+        ]
+    },
+    {
+        category: "Live Projects",
+        links: [
+            { name: "Alsuwa", url: "https://alsuwa.com" },
+            { name: "Shea254", url: "https://shea254.com" },
+            { name: "QingJia", url: "https://qingjiasafaris.com" },
+            { name: "Simian", url: "https://simiansafaris.com" },
+            { name: "Bolomba", url: "https://bolombametals.com" },
+            { name: "Twangiza", url: "https://twangizametals.com" },
+            { name: "Portfolio", url: "https://alexaaqil.netlify.app/" },
+        ]
+    },
+    {
+        category: "Reads",
+        links: [
+            { name: "daily.dev", url: "https://daily.dev" },
+            { name: "Medium", url: "https://medium.com" },
+        ]
+    },
+    {
+        category: "Fiscals",
+        links: [
+            { name: "KSL Trader", url: "https://trading.kingdomsecurities.co.ke/tradeweb/#!/login/" },
+        ]
+    }
+  ],
+} as const;
+
+
+
+
+
+
+
+
